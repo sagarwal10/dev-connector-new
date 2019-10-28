d13fa8f5c002b96a7f32e8bbb09e26d0cbb02eba
Shalabh Agarwal <sagarwal10@hotmail.com>
2019-10-28 16:12:24 -0700

    Added code to delete post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,7 +4,8 @@ import {
   GET_POSTS,
   POST_ERROR, 
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from './types';
 
 
@@ -27,10 +28,36 @@ export const getPosts = () => async dispatch => {
 } 
 
 
+// Add post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  try {
+    const res = await axios.post('/api/posts', formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Post Created'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText,  
+                 status: err.response.status }
+    });
+  }
+}
+ 
 // Delete post
 export const deletePost = id => async dispatch => {
   try {
-    const res = await axios.delete(`/api/posts/${id}`);
+    await axios.delete(`/api/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
@@ -46,7 +73,6 @@ export const deletePost = id => async dispatch => {
     });
   }
 }
- 
 // Add like
 export const addLike = id => async dispatch => {
   try {
